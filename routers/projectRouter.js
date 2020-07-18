00571ff0aba2a52999dd01b065d2bbe380d2160c
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-07-17 22:28:15 -0400

    created new project

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -4,6 +4,9 @@ const Projects = require('../data/helpers/projectModel');
 
 const router = express.Router();
 
+// ...............................
+// Get projects
+// ...............................
 router.get('/', (req, res) => {;
     Projects.get()
         .then(projects => {
@@ -29,5 +32,35 @@ router.get('/:id', (req, res) => {
             res.status(404).json({ message: "the project could not be found"});
         });
 });
+
+// ...............................
+// Create project
+// ...............................
+
+router.post('/', (req, res) => {
+    Projects.insert(req.body)
+        .then(post => {
+            if(!req.body.name || !req.body.description) {
+                res.status(400).json({ error: "Please provide name and description for project"})
+            } else {
+                res.status(201).json(post)
+            }
+        })
+        .catch(error => {
+            res.statys(500).json({ error: "There was a problem saving data"});
+        });
+});
+
+
+// ...............................
+// Update project
+// ...............................
+
+
+
+
+// ...............................
+// Delete project
+// ...............................
 
 module.exports = router;
