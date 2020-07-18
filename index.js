9d7f3fc72bcd951dfd09138327917f93f84426a5
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-07-17 21:37:45 -0400

    server is up and running

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.json( 'I am at your server!' );
+});
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+    console.log('Server is on', PORT);
+});
